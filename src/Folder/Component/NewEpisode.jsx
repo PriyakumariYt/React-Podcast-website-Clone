a8ad03955ebed8c10b4bdf9fbb08617bb7ea5718
Priya Kumari <127418517+PriyakumariYt@users.noreply.github.com>
2023-06-01 14:28:41 +0530

    new add

---
+++ src/Folder/Component/NewEpisode.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+function NewEpisode() {
+  return (
+    <>
+      <div className="Containers">
+        <div className="text">
+          <h2>New Episode!</h2>
+          <h1>
+            The DiviNation <br />
+            Podcast
+          </h1>
+          <p>
+            A podcast series about life in the Aussie bush, mateship & the RFDS
+            in rural communities. Catch up on our latest episode on your
+            favourite platform today. Start listening now. 24-Hour Emergency
+            Service.
+          </p>
+          <div className="play">
+            <i className="fa-solid fa-play"></i>
+          </div>
+          <div className="buttons-1">Play Now</div>
+          <div className="buttons-2">Subscribe Now</div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default NewEpisode;
